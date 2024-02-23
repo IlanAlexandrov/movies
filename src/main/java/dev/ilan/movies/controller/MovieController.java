dde15c68176ae44ae0121eb99d3f0bb31c630aa3
Ilan Alexandrov <ilan@adscale.com>
2024-02-23 16:33:14 +0200

    Creating controller service and repository

--- src/main/java/dev/ilan/movies/controller/MovieController.java
+++ src/main/java/dev/ilan/movies/controller/MovieController.java
@@ -1,14 +1,26 @@
 package dev.ilan.movies.controller;
 
 
+import dev.ilan.movies.Modal.Movie;
+import dev.ilan.movies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MovieController {
+import java.util.List;
 
-	@GetMapping("/movies")
-	public String apiRoot(){
-		return "Welcome to the movies API!";
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+	@Autowired
+	private MovieService movieService;
+
+	@GetMapping
+	public ResponseEntity<List<Movie>> allMovies() {
+		List<Movie> allMovies = movieService.getAllMovies();
+		return new ResponseEntity<>(allMovies, HttpStatus.OK);
 	}
 }
